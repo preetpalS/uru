3c0579f52f196a86e5d906a9f4eb217fdd0f06f8
Jon <jon.forums@gmail.com>
2013-05-20 21:13:12 -0400

    Teach `ls` to recognize --verbose. Closes #17

--- command/ls.go
+++ command/ls.go
@@ -26,6 +26,14 @@ func List(ctx *env.Context) {
 		return
 	}
 
+	verbose := false
+	for _, v := range ctx.CmdArgs() {
+		if v == `--verbose` {
+			verbose = true
+			break
+		}
+	}
+
 	tag, _, err := env.CurrentRubyInfo(ctx)
 	if err != nil {
 		fmt.Printf("---> Unable to list rubies; try again\n")
@@ -44,5 +52,9 @@ func List(ctx *env.Context) {
 			desc = fmt.Sprintf("%.65s...", desc)
 		}
 		fmt.Printf(" %s %10.10s: %s\n", me, ri.TagLabel, desc)
+		if verbose {
+			indent := fmt.Sprintf("%15.15s", ``)
+			fmt.Printf("%s Home: %s\n%s GemHome: %s\n\n", indent, ri.Home, indent, ri.GemHome)
+		}
 	}
 }
