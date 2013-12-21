7b04e31da8ae720a014738884927c3a14dc0edb5
Jon <jon.forums@gmail.com>
2013-12-21 09:31:52 -0500

    Teach uru to remove all registered rubies
    Remove all registered rubies by invoking uru like
    
      uru admin rm --all

--- command/admin_rm.go
+++ command/admin_rm.go
@@ -14,7 +14,7 @@ func init() {
 	AdminCmdRegistry["rm"] = Command{
 		Name:    "rm",
 		Aliases: []string{"rm", "del"},
-		Usage:   "admin rm TAG",
+		Usage:   "admin rm TAG | admin rm --all",
 		HelpMsg: "deregister a ruby installation",
 		Eg:      `admin rm 193p193`}
 }
@@ -25,44 +25,66 @@ func adminRemove(ctx *env.Context) {
 		os.Exit(1)
 	}
 
-	cmd := ctx.CmdArgs()[0]
-	tags, err := env.TagLabelToTag(ctx, cmd)
-	if err != nil {
-		fmt.Printf("---> unable to find registered ruby matching `%s`\n", cmd)
-		os.Exit(1)
-	}
-
-	tag := ``
-	if len(tags) == 1 {
-		// XXX less convoluted way to get the key of a 1 element map?
-		for t, _ := range tags {
-			tag = t
+	var rmAll bool
+	var tagLabel string
+	for _, v := range ctx.CmdArgs() {
+		if v == `--all` {
+			rmAll = true
+			tagLabel = `all`
 			break
 		}
-	} else {
-		// multiple rubies match the given tag label, ask the user for the
-		// correct one.
-		tag, err = env.SelectRubyFromList(tags, cmd, `deregister`)
+	}
+
+	if rmAll {
+		resp, err := env.UIYesConfirm("\nOK to deregister all rubies?")
 		if err != nil {
+			fmt.Println("---> Unable to understand your response. Try again")
+			return
+		}
+		if resp == `N` {
+			return
+		}
+		ctx.Registry.Rubies = make(env.RubyMap, 4)
+	} else {
+		tagLabel = ctx.CmdArgs()[0]
+		tags, err := env.TagLabelToTag(ctx, tagLabel)
+		if err != nil {
+			fmt.Printf("---> unable to find registered ruby matching `%s`\n", tagLabel)
 			os.Exit(1)
 		}
+
+		tag := ``
+		if len(tags) == 1 {
+			// XXX less convoluted way to get the key of a 1 element map?
+			for t, _ := range tags {
+				tag = t
+				break
+			}
+		} else {
+			// multiple rubies match the given tag label, ask the user for the
+			// correct one.
+			tag, err = env.SelectRubyFromList(tags, tagLabel, `deregister`)
+			if err != nil {
+				os.Exit(1)
+			}
+		}
+
+		rb := ctx.Registry.Rubies[tag]
+
+		resp, err := env.UIYesConfirm(fmt.Sprintf("\nOK to deregister `%s`?", rb.Description))
+		if err != nil {
+			fmt.Println("---> Unable to understand your response. Try again")
+			return
+		}
+		if resp == `N` {
+			return
+		}
+
+		delete(ctx.Registry.Rubies, tag)
 	}
 
-	rb := ctx.Registry.Rubies[tag]
-
-	resp, err := env.UIYesConfirm(fmt.Sprintf("\nOK to deregister `%s`?", rb.Description))
+	err := env.MarshalRubies(ctx)
 	if err != nil {
-		fmt.Println("---> Unable to understand your response. Try again")
-		return
-	}
-	if resp == `N` {
-		return
-	}
-
-	delete(ctx.Registry.Rubies, tag)
-
-	err = env.MarshalRubies(ctx)
-	if err != nil {
-		fmt.Printf("---> Failed to remove `%s`. Try again", tag)
+		fmt.Printf("---> Failed to remove `%s`. Try again", tagLabel)
 	}
 }
