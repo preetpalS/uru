4d694c121a15cec5cb98c93369c833c5be604095
Jon <jon.forums@gmail.com>
2013-12-28 18:14:04 -0500

    Add env utility tests

--- env/util_test.go
+++ env/util_test.go
@@ -10,6 +10,29 @@ import (
 	"testing"
 )
 
+var (
+	testRubies = []Ruby{
+		{
+			ID:          `2.1.1-p1`,
+			TagLabel:    `211p1`,
+			Exe:         `ruby`,
+			Home:        `/home/fake/.rubies/ruby-2.1.0/bin`,
+			GemHome:     `/home/fake/.gem/ruby/2.1.0`,
+			Description: `ruby 2.1.1p1 (2013-12-27 revision 44443) [i386-mingw32]`,
+		},
+		{
+			ID:          `1.7.9`,
+			TagLabel:    `179`,
+			Exe:         `jruby`,
+			Home:        `C:\Apps\rubies\jruby\bin`,
+			GemHome:     ``,
+			Description: `jruby 1.7.9 (1.9.3p392) 2013-12-06 87b108a on Java HotSpot(TM) 64-Bit Server VM 1.7.0_45-b18 [Windows 8-amd64]`,
+		},
+	}
+	testLabels = []string{`211`, `179`}
+	testTags   = []string{`2292638747`, `444332046`}
+)
+
 func TestStringSplitPath(t *testing.T) {
 	prevPath := os.Getenv(`PATH`)
 	newPath := []string{`A`, `B`, `C`, `D`}
@@ -27,20 +50,41 @@ func TestStringSplitPath(t *testing.T) {
 
 func TestNewTag(t *testing.T) {
 	ctx := NewContext()
-	testRuby := Ruby{
-		ID:          `2.1.1-p1`,
-		TagLabel:    `211p1`,
-		Exe:         `ruby`,
-		Home:        `/home/fake/.rubies/ruby-2.1.0/bin`,
-		GemHome:     `/home/fake/.gem/ruby/2.1.0`,
-		Description: `ruby 2.1.1p1 (2013-12-27 revision 44443) [i386-mingw32]`,
-	}
-	tag := `2292638747`
 
-	rv, _ := NewTag(ctx, testRuby)
-	if rv != tag {
+	rv, _ := NewTag(ctx, testRubies[0])
+	if rv != testTags[0] {
 		t.Errorf("NewTag not returning correct value\n  want: `%v`\n  got: `%v`",
-			tag,
+			testTags[0],
 			rv)
+	}
+}
+
+func TestTagLabelToTag(t *testing.T) {
+	ctx := NewContext()
+	ctx.Registry = RubyRegistry{
+		Version: RubyRegistryVersion,
+		Rubies: RubyMap{
+			testTags[0]: testRubies[0],
+			testTags[1]: testRubies[1],
+		},
+	}
+
+	// nonexistent tag label test
+	tags, err := TagLabelToTag(ctx, `200`)
+	if err == nil {
+		t.Error("TagLabelToTag() should return error for nonexistent tag label")
+	}
+
+	// valid tag label tests
+	for i, rb := range testRubies {
+		tags, err = TagLabelToTag(ctx, testLabels[i])
+		if err != nil {
+			t.Error("TagLabelToTag() should not return error for valid tag label")
+		}
+		if tags[testTags[i]].ID != rb.ID {
+			t.Errorf("TagLabelToTag() not returning correct value\n  want: `%v`\n  got: `%v`",
+				rb.ID,
+				tags[testTags[i]].ID)
+		}
 	}
 }
