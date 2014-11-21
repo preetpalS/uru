b0649226ee512c7e17f49a2d07e73ccde40ae78c
Jon <jon.forums@gmail.com>
2014-11-21 00:27:25 -0500

    Add Dispatch test for CommandRouter

--- command/router_test.go
+++ command/router_test.go
@@ -4,6 +4,8 @@
 package command
 
 import (
+	"bytes"
+	"fmt"
 	"regexp"
 	"testing"
 
@@ -24,6 +26,39 @@ func TestRouterConfig(t *testing.T) {
 		t.Errorf("Incorrect CommandRouter handler count\n  want: `%v`\n  got: `%v`\n",
 			count,
 			num)
+	}
+}
+
+func TestRouterDispatch(t *testing.T) {
+	out := new(bytes.Buffer)
+
+	ctx := env.NewContext()
+
+	defExpected := "default_test"
+	r := NewRouter(func(*env.Context) { fmt.Fprintf(out, "%s", defExpected) })
+	r.Handle([]string{`admin`}, func(*env.Context) { fmt.Fprintf(out, "%s", "admin_test") })
+	r.Handle([]string{`gem`}, func(*env.Context) { fmt.Fprintf(out, "%s", "gem_test") })
+
+	// test registered command routing
+	for _, c := range []string{`admin`, `gem`} {
+		expected := fmt.Sprintf("%s_test", c)
+		r.Dispatch(ctx, c)
+		result := out.String()
+		if expected != result {
+			t.Errorf("Command route dispatch failed\n  want: `%v`\n  got: `%v`\n",
+				expected,
+				result)
+		}
+		out.Reset()
+	}
+
+	// test default routing for unknown command
+	r.Dispatch(ctx, "ruby2")
+	result := out.String()
+	if defExpected != result {
+		t.Errorf("Default command route dispatch failed\n  want: `%v`\n  got: `%v`\n",
+			defExpected,
+			result)
 	}
 }
 
